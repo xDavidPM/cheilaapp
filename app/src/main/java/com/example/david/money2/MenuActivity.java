6fce1820c27b1cce8a3c8ac7bc0cc86b6b571f06
xDavidPM <davidpazmen21@gmail.com>
2016-12-16 01:44:30 -0500

    frag1 2 y objet

--- app/src/main/java/com/example/david/money2/MenuActivity.java
+++ app/src/main/java/com/example/david/money2/MenuActivity.java
@@ -3,7 +3,7 @@ package com.example.david.money2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.View;  
 import android.widget.LinearLayout;
 
 /**
--- app/src/main/java/com/example/david/money2/ObjetiveActivity.java
+++ app/src/main/java/com/example/david/money2/ObjetiveActivity.java
@@ -1,17 +1,19 @@
 package com.example.david.money2;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 /**
  * Created by david on 15/12/16.
  */
 
 public class ObjetiveActivity extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.objetive_activity);
+
     }
 }
