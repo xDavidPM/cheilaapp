38e2bd61f06e6299ede9f59444d559ed821e7ec2
xDavidPM <davidpazmen21@gmail.com>
2016-12-15 20:29:26 -0500

    ingress menu

--- app/src/main/java/com/example/david/money2/MenuActivity.java
+++ app/src/main/java/com/example/david/money2/MenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.david.money2;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,6 +8,11 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class MenuActivity extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_actitivy);
+    }
 }
+
 
