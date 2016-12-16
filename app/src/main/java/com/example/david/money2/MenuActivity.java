6bacb0daac99cc1240b84ff89ec1d09e0d599964
xDavidPM <davidpazmen21@gmail.com>
2016-12-15 21:22:38 -0500

    save

--- app/src/main/java/com/example/david/money2/MenuActivity.java
+++ app/src/main/java/com/example/david/money2/MenuActivity.java
@@ -1,17 +1,39 @@
 package com.example.david.money2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by david on 15/12/16.
  */
 
 public class MenuActivity extends AppCompatActivity {
+
+    Button save;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_actitivy);
+
+        save = (Button) findViewById(R.id.btn_save);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                Intent i;
+
+                i = new Intent(MenuActivity.this, SaveActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
     }
 }
 
---
+++ app/src/main/java/com/example/david/money2/SaveActivity.java
@@ -0,0 +1,17 @@
+package com.example.david.money2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by david on 15/12/16.
+ */
+
+public class SaveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.save_activity);
+    }
+}
